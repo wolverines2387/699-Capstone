8cac372ed466b64b71635cb988609cbc6a3bdc10
wolverines2387 <ryanwt@umich.edu>
2023-11-25 16:35:57 -0700

    update

--- create_choropleth_map.py
+++ create_choropleth_map.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8d1d5e9878fc7fc345a022f8c66606ebe951e6ff20d8668be7926bac4908e3a
-size 5551
+oid sha256:ba2c013123fccf3592c8270df9f863851f783970c29b04c6b94e088cd890146f
+size 5536
