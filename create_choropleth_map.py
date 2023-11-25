7d4409ccfeb5f1f7e23bf3fce35ec117e4dd091b
wolverines2387 <ryanwt@umich.edu>
2023-11-25 16:32:44 -0700

    update file references

--- create_choropleth_map.py
+++ create_choropleth_map.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7287d38941c87c663f728614f6450b9e8481a030434f86b65a83b802d5c724f
-size 5553
+oid sha256:c8d1d5e9878fc7fc345a022f8c66606ebe951e6ff20d8668be7926bac4908e3a
+size 5551
--- neighborhood_to_avg_graph.py
+++ neighborhood_to_avg_graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c490630eff74ef64d9d8aac8f2e506ed96cc7b29e92275235947a8878ba27eac
-size 5946
+oid sha256:321e5dd5c69b75d2d08e2a378aa781e2bd064c4d8f3404fcfe0cb77397e0ba95
+size 5944
