24fe77bd9c7034d461100d5da228c0a51f33d071
wolverines2387 <ryanwt@umich.edu>
2023-11-25 19:36:19 -0700

    update

--- import_airbnb.py
+++ import_airbnb.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b77e212dd299b6d23127765ca722d8ccd44c513518f117f082aef338fa9abd36
-size 4644
+oid sha256:aed1e56097a42aaf98ab79c957ed1af479ab702b86e964ca2ef59cdead5fc979
+size 3077
