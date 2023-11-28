c55474a393d1530284ec2746947bd0cbbb16bb5c
wolverines2387 <126736758+wolverines2387@users.noreply.github.com>
2023-11-28 03:28:46 +0000

    push inserts of our graphs (still need to connect)

--- price_predictor_dashboard.py
+++ price_predictor_dashboard.py
@@ -34,6 +34,25 @@ sidebar_selection = st.sidebar.radio(
      'chicago', 'austin', 'seattle', 'rochester', 'san-francisco'],
 )
 
+###Figure out where to place this:
+def get_neighborhoods(directory):
+    cities = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
+    city_dir = {}
+    
+    for city in cities:
+        city_file_path = os.path.join(directory, city,'listings.csv.gz')
+        city_df = pd.read_csv(city_file_path)
+        neighbourhood_list = city_df.neighbourhood_cleansed.unique().tolist()
+        
+        city_dir[city] = neighbourhood_list
+    
+    return city_dir
+
+directory = 'data'
+neighborhoods = get_neighborhoods(directory)
+
+#################################################################
+
 @st.cache(ttl=3*60*60, suppress_st_warning=True)
 def get_data():
     US_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
@@ -167,6 +186,246 @@ def get_testing_data(County):
         data['new_tests_rolling'] = data['new_tests'].fillna(0).rolling(14).mean()
         data['testing_positivity_rolling'] = (data['new_positive_tests_rolling'] / data['new_tests_rolling'])*100
         return data['new_tests_rolling'], data['testing_positivity_rolling'].iloc[-1:].values[0]
+    
+#### Our graphs before I comment all this other stuff out....
+
+def choropleth(city, neighborhood):
+    
+    #Get GeoJson File and format
+    geo_json_file_path = city_file_path = os.path.join('geojson_data', city,'neighbourhoods.geojson')
+    geo_json = json.load(open(geo_json_file_path))
+    
+    #Get HUD Data
+    read_path = 'Chicago_Data/agg_choropleth_data/hud_neighborhoods_agg.pkl' ###CHANGE THIS IN PERM CODE!
+
+    agg_neighborhoods_df = pd.read_pickle(read_path)
+
+    agg_neighborhoods_df_city = agg_neighborhoods_df[agg_neighborhoods_df.city == city]
+    agg_neighborhoods_df_city.rename(columns={'total_units': 'Affordable Units Available', 'pct_occupied': 
+                                              '% Affordable Units Occupied', 'rent_per_month': 'Avg Rent Per Month', 
+                                              'pct_lt50_median':'% Very Low Income', 'tpoverty':'% In Poverty'}, 
+                                            inplace=True)
+    
+    #Get Lat Lons for City Center
+    list_of_cities_lat_lon = sorted([('columbus', 39.9612, -82.9988),
+                  ('los-angeles', 34.0549, -118.2426),
+                  ('new-york-city', 40.7128, -74.0060),
+                  ('fort-worth', 32.7555, -97.3308),
+                  ('boston',42.3601,-71.0589),
+                  ('broward-county', 26.1224, -80.1373),
+                  ('chicago', 41.8781, -87.6232),
+                  ('austin', 30.2672, -97.7431),
+                  ('seattle', 47.6061, -122.3328),
+                  ('rochester', 43.1566, -77.6088),
+                  ('san-francisco', 37.7749, -122.4194)])
+
+    # Initialize the variable to None
+    matching_lat_lon = None
+
+    # Iterate over the list of tuples
+    for city_lat_lon in list_of_cities_lat_lon:
+        # If the first element of the tuple matches the city variable
+        if city_lat_lon[0] == city:
+            # Assign the matching tuple to the variable
+            matching_tuple = city_lat_lon
+            break  # Exit the loop
+
+    city_center_lat_lons = matching_tuple
+    city_center_lat = city_center_lat_lons[1]
+    city_center_lon = city_center_lat_lons[2]
+    
+    # Selected neighborhood
+    pin_point_df = pd.read_pickle('pin_point_coordinates.pkl')
+    neigh_val = pin_point_df.coordinates[(pin_point_df['city'] == city) & (pin_point_df['neighborhood'] == neighborhood)]
+    neigh_lat_lons = neigh_val.values
+
+    
+    ## Generate Figure
+    hover_text = neighborhood
+
+    # Loctation Hover Tet
+    for _, row in agg_neighborhoods_df_city.iterrows():
+        for column in row.index:
+            hover_text += f"<br>{column}: {row[column]}"
+
+    # Choropleth
+    fig = px.choropleth_mapbox(agg_neighborhoods_df_city.round(2),
+                               geojson=geo_json,
+                               locations="neighborhood",
+                               featureidkey='properties.neighbourhood',
+                               color='% Affordable Units Occupied',
+                               color_continuous_scale=px.colors.sequential.Oryel,
+                               range_color=(0, 100),
+                               mapbox_style="carto-positron",
+                               zoom=9, center={"lat": city_center_lat, "lon": city_center_lon},
+                               opacity=0.7,
+                               hover_name='% Affordable Units Occupied',
+                               hover_data=['Affordable Units Available', '% Affordable Units Occupied', 'Avg Rent Per Month', 
+                                           '% Very Low Income','% In Poverty'],  
+                               title='Neighborhood Affordability Data'
+                              )
+
+    fig.update_layout(dragmode=False)
+
+    # Property Address Marker
+    fig.add_trace(go.Scattermapbox(
+        lat=[neigh_lat_lons[0][0]],  
+        lon=[neigh_lat_lons[0][1]],  
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=10,  
+            color='green', 
+        ),
+        text=[hover_text], 
+        hoverinfo='text'
+    ))
+    
+    return fig.show()
+
+def get_neighborhood_to_avg(city, city_neighborhood):
+    read_path = 'Chicago_Data/agg_choropleth_data/hud_neighborhoods_agg.csv'
+
+    agg_neighborhoods_df = pd.read_pickle(read_path)
+
+    agg_neighborhoods_df_city = agg_neighborhoods_df[agg_neighborhoods_df.city == city]
+    
+    agg_neighborhoods_df_city = agg_neighborhoods_df_city.drop(columns = ['city'])
+    agg_neighborhoods_df_city = agg_neighborhoods_df_city.set_index('neighborhood')
+    agg_neighborhoods_df_city.loc['Average Values'] = agg_neighborhoods_df_city.mean()
+    agg_neighborhoods_df_city.loc['Median Values'] = agg_neighborhoods_df_city.median()
+    agg_neighborhoods_df_city = agg_neighborhoods_df_city.reset_index().round(2)
+    
+    columns = agg_neighborhoods_df_city.columns.to_list()
+    neighborhood = columns[0]
+    other_columns = columns[1::]
+    neighborhood_percentiles = ['25th Percentile','75th Percentile',"Max","Min"]
+    percentiles = ['25%', '75%','max','min']
+
+    for column_value, percentile in zip(neighborhood_percentiles, percentiles):
+        value_list = [column_value]
+        for column in other_columns:
+            agg_neighborhoods_df_city[column].apply(pd.to_numeric)
+            value = agg_neighborhoods_df_city[column].describe()
+            value_list.append(value[percentile])
+        agg_neighborhoods_df_city.loc[len(agg_neighborhoods_df_city)]=value_list
+        
+    avg_vals = "Average Values"
+    hud_filtered_df = agg_neighborhoods_df_city.loc[agg_neighborhoods_df_city['neighborhood'].isin([avg_vals,city_neighborhood])]
+    
+    hud_filtered_df = hud_filtered_df.groupby('neighborhood').agg({
+        'total_units': 'sum',
+        'pct_occupied': 'mean',
+        'number_reported': 'sum',
+        'pct_reported': 'mean',
+        'months_since_report': 'mean',
+        'pct_movein': 'mean',
+        'people_per_unit': 'mean',
+        'people_total' : 'sum',
+        'rent_per_month' : 'mean',
+        'spending_per_month' : 'mean', 
+        'hh_income' : 'mean',
+        'person_income' : 'mean', 
+        'pct_lt5k' : 'mean',
+        'pct_5k_lt10k' :'mean',
+        'pct_10k_lt15k' : 'mean',
+        'pct_15k_lt20k' : 'mean',
+        'pct_ge20k' : 'mean',
+        'pct_wage_major' : 'mean',
+        'pct_welfare_major' : 'mean',
+        'pct_other_major' : 'mean',
+        'pct_median' : 'mean', 
+        'pct_lt50_median' : 'mean',
+        'pct_lt30_median' :'mean',
+        'pct_2adults' : 'mean',
+        'pct_1adult' : 'mean', 
+        'pct_female_head' : 'mean',
+        'pct_female_head_child' : 'mean',
+        'pct_disabled_lt62' : 'mean',
+        'pct_disabled_ge62' : 'mean',
+        'pct_disabled_all' : 'mean',
+        'pct_lt24_head'  : 'mean',
+        'pct_age25_50' : 'mean', 
+        'pct_age51_61'  : 'mean',
+        'pct_age62plus'  : 'mean',
+        'pct_age85plus'  : 'mean',
+        'pct_minority'  : 'mean',
+        'pct_black_nonhsp'  : 'mean',
+        'pct_native_american_nonhsp'  : 'mean',
+        'pct_asian_pacific_nonhsp'  : 'mean',
+        'pct_white_nothsp'  : 'mean', 
+        'pct_black_hsp'  : 'mean',
+        'pct_wht_hsp': 'mean',
+        'pct_oth_hsp': 'mean',
+        'pct_hispanic': 'mean',
+        'pct_multi' : 'mean',
+        'months_waiting'  : 'mean', 
+        'months_from_movein'  : 'mean',
+        'pct_utility_allow'  : 'mean',
+        'ave_util_allow'  : 'mean',
+        'pct_bed1'  : 'mean',
+        'pct_bed2'  : 'mean', 
+        'pct_bed3'  : 'mean', 
+        'pct_overhoused'  : 'mean',
+        'tpoverty'  : 'mean', 
+        'tminority'  : 'mean',
+        'tpct_ownsfd'  : 'mean'
+    }).reset_index()
+    
+    hud_filtered_df = hud_filtered_df[['neighborhood', 'total_units', 'pct_occupied','people_per_unit', 'person_income','months_waiting','pct_overhoused', 'tpoverty','tpct_ownsfd']].round(2) 
+    
+    columns = ['total_units','pct_occupied','people_per_unit',
+           'person_income','months_waiting',
+           'tpoverty','tpct_ownsfd']
+
+    titles = ['Total Units in Area w/ Government Subsidies', 'Occupied Units in Area as a % of Units Available', 
+              'Average Size of Household in Area', 'Average Household Income per Person per Year in Area', 
+              'Months on Waiting List in Area', '% of Surrounding Area in Poverty', 
+              '% Surrounding Area Single Family Owners']
+
+
+    plots = []
+
+    for i, column in enumerate(columns):
+
+        all_neighborhood_vals = alt.Chart(agg_neighborhoods_df_city).mark_boxplot(extent='min-max', color='teal', size=30).encode(
+            x=alt.X(column+':Q'),
+            tooltip=alt.Tooltip(column+':Q')
+        ).properties(
+            title=titles[i], 
+            width=600, 
+            height=200
+        )
+
+
+        neighborhood_vals = alt.Chart(hud_filtered_df[hud_filtered_df['neighborhood'].isin([city_neighborhood, 'Average Values'])]).mark_point(
+            filled=True,
+            size=100 
+        ).encode(
+            x=alt.X(column+':Q'),
+            color=alt.Color('neighborhood:N', scale=alt.Scale(domain=[city_neighborhood, 'Average Values'], range=['red', 'blue'])),
+            tooltip=[alt.Tooltip('neighborhood:N'), alt.Tooltip(column+':Q')]
+        )
+
+
+        box_and_marks = alt.layer(all_neighborhood_vals, neighborhood_vals)
+
+
+        plots.append(box_and_marks)
+
+
+    chart = alt.vconcat(*plots)
+
+
+    chart = chart.configure_view(stroke='transparent', fill='white').configure_title(color='darkgrey').configure_axis(
+        labelColor='darkgrey',
+        titleColor='darkgrey'
+    )
+
+
+    return chart.display()
+
+
+######################################################################################
 
 
 def plot_county(county):
